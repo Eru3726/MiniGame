89d83deb05397b1958abf7b05353220119f149fe
Eru <eru.gamecreator@gmail.com>
2023-10-15 00:46:01 +0900

    ある程度完成

--- MiniGame/Assets/Scripts/NumberPlate/NumText.cs
+++ MiniGame/Assets/Scripts/NumberPlate/NumText.cs
@@ -6,12 +6,14 @@ public class NumText : MonoBehaviour
     public Image bg;
     public Text text;
     public int ansInt;
+    public bool ansFlg = false;
 
     void Start()
     {
         bg = GetComponent<Image>();
         text = GetComponentInChildren<Text>();
         bg.color = Color.white;
+        ansFlg = true;
     }
 
     void Update()
--- MiniGame/Assets/Scripts/NumberPlate/NumberPlateManager.cs
+++ MiniGame/Assets/Scripts/NumberPlate/NumberPlateManager.cs
@@ -4,17 +4,118 @@ using UnityEngine.UI;
 
 public class NumberPlateManager : MonoBehaviour
 {
-    private int[,] defaultNum = new int[,]
+    private int[,,] defaultNum = new int[,,]
     {
-        {1,4,7,2,5,8,3,6,9},
-        {2,5,8,3,6,9,4,7,1},
-        {3,6,9,4,7,1,5,8,2},
-        {4,7,1,5,8,2,6,9,3},
-        {5,8,2,6,9,3,7,1,4},
-        {6,9,3,7,1,4,8,2,5},
-        {7,1,4,8,2,5,9,3,6},
-        {8,2,5,9,3,6,1,4,7},
-        {9,3,6,1,4,7,2,5,8}
+        {
+            {1,4,7,2,5,8,3,6,9},
+            {2,5,8,3,6,9,4,7,1},
+            {3,6,9,4,7,1,5,8,2},
+            {4,7,1,5,8,2,6,9,3},
+            {5,8,2,6,9,3,7,1,4},
+            {6,9,3,7,1,4,8,2,5},
+            {7,1,4,8,2,5,9,3,6},
+            {8,2,5,9,3,6,1,4,7},
+            {9,3,6,1,4,7,2,5,8}
+        },
+        {
+            {7,2,6,3,4,8,1,9,5},
+            {5,9,4,1,2,7,3,8,6},
+            {3,1,8,6,5,9,7,4,2},
+            {4,7,3,5,1,6,8,2,9},
+            {6,8,2,9,7,3,4,5,1},
+            {9,5,1,2,8,4,6,7,3},
+            {1,4,5,7,6,2,9,3,8},
+            {2,3,7,8,9,1,5,6,4},
+            {8,6,9,4,3,5,2,1,7}
+        },
+        {
+            {2,3,4,8,1,5,7,6,9},
+            {5,8,1,9,7,6,4,2,3},
+            {9,6,7,4,2,3,5,8,1},
+            {3,4,2,6,8,1,9,5,7},
+            {8,7,5,3,4,9,6,1,2},
+            {6,1,9,2,5,7,3,4,8},
+            {1,5,6,7,3,2,8,9,4},
+            {4,2,3,5,9,8,1,7,6},
+            {7,9,8,1,6,4,2,3,5}
+        },
+        {
+            {2,9,7,8,4,5,1,6,3},
+            {6,5,4,2,1,3,9,8,7},
+            {8,3,1,7,6,9,2,5,4},
+            {3,4,6,5,9,2,7,1,8},
+            {9,2,8,6,7,1,3,4,5},
+            {1,7,5,4,3,8,6,9,2},
+            {7,6,3,1,8,4,5,2,9},
+            {4,1,2,9,5,7,8,3,6},
+            {5,8,9,3,2,6,4,7,1}
+        },
+        {
+            {3,1,8,9,2,4,7,5,6},
+            {7,4,9,6,8,5,3,2,1},
+            {6,2,5,1,7,3,8,9,4},
+            {5,9,7,8,6,2,4,1,3},
+            {2,3,1,7,4,9,6,8,5},
+            {8,6,4,3,5,1,9,7,2},
+            {1,7,3,5,9,6,2,4,8},
+            {9,5,2,4,3,8,1,6,7},
+            {4,8,6,2,1,7,5,3,9}
+        },
+        {
+            {1,5,7,9,6,4,2,3,8},
+            {9,6,4,2,8,3,1,5,7},
+            {3,2,8,5,7,1,4,9,6},
+            {7,8,6,4,2,9,3,1,5},
+            {4,3,9,1,5,8,6,7,2},
+            {2,1,5,6,3,7,9,8,4},
+            {5,4,3,7,9,2,8,6,1},
+            {8,7,2,3,1,6,5,4,9},
+            {6,9,1,8,4,5,7,2,3}
+        },
+        {
+            {3,1,5,8,4,2,6,9,7},
+            {7,9,4,5,6,1,3,8,2},
+            {2,8,6,7,9,3,4,5,1},
+            {6,3,9,1,2,8,7,4,5},
+            {4,7,1,9,5,6,8,2,3},
+            {8,5,2,4,3,7,9,1,6},
+            {5,4,7,3,1,9,2,6,8},
+            {9,2,3,6,8,5,1,7,4},
+            {1,6,8,2,7,4,5,3,9}
+        },
+        {
+            {1,9,7,8,6,3,4,2,5},
+            {8,4,6,9,2,5,7,1,3},
+            {5,3,2,1,4,7,8,6,9},
+            {2,5,4,7,9,1,3,8,6},
+            {3,6,8,2,5,4,1,9,7},
+            {9,7,1,6,3,8,2,5,4},
+            {4,8,3,5,1,9,6,7,2},
+            {6,1,5,4,7,2,9,3,8},
+            {7,2,9,3,8,6,5,4,1}
+        },
+        {
+            {4,9,8,7,3,2,5,6,1},
+            {6,1,2,9,5,4,7,8,3},
+            {3,5,7,1,6,8,4,2,9},
+            {2,3,5,6,1,9,8,7,4},
+            {7,8,4,3,2,5,1,9,6},
+            {9,6,1,8,4,7,2,3,5},
+            {5,7,6,4,8,3,9,1,2},
+            {8,4,3,2,9,1,6,5,7},
+            {1,2,9,5,7,6,3,4,8}
+        },
+        {
+            {4,2,8,5,6,3,1,7,9},
+            {1,7,9,2,4,8,5,3,6},
+            {3,5,6,1,7,9,2,4,8},
+            {8,4,5,3,9,2,6,1,7},
+            {6,1,7,4,8,5,9,2,3},
+            {9,3,2,7,1,6,4,8,5},
+            {7,9,4,6,3,1,8,5,2},
+            {2,6,3,8,5,4,7,9,1},
+            {5,8,1,9,2,7,3,6,4}
+        },
     };
 
     private int[,] ansNum = new int[9, 9];
@@ -54,19 +155,38 @@ public class NumberPlateManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if(nowImage != null) nowImage.color = Color.white;
-            if (numText != null) numText = null;
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
 
             if (hit.collider != null && hit.collider.gameObject.TryGetComponent<NumText>(out NumText numT))
             {
+                if (nowImage != null) nowImage.color = Color.white;
                 nowImage = numT.bg;
                 nowImage.color = Color.green;
                 numText = numT;
 
             }
         }
+    }
+
+    /// <summary>
+    /// テンプレの初期化
+    /// </summary>
+    /// <returns></returns>
+    private int[,] NumTempInit()
+    {
+        int[,] swappedArray = new int[9,9];
+        int rand = Random.Range(0, 10);
+
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < 9; j++)
+            {
+                swappedArray[i, j] = defaultNum[rand, i, j];
+            }
+        }
+
+        return swappedArray;
     }
 
     /// <summary>
@@ -80,7 +200,7 @@ public class NumberPlateManager : MonoBehaviour
             do
             {
                 randomNum = Random.Range(0, 81);
-            } while (selectedNumbers.Contains(randomNum)); 
+            } while (selectedNumbers.Contains(randomNum));
 
             selectedNumbers.Add(randomNum);
         }
@@ -298,11 +418,12 @@ public class NumberPlateManager : MonoBehaviour
 
     private void NumTextAns(int[,] array)
     {
-        for(int i = 0; i < 9; i++)
+        for (int i = 0; i < 9; i++)
         {
-            for(int j = 0; j < 9; j++)
+            for (int j = 0; j < 9; j++)
             {
                 numTexts[i, j].ansInt = array[i, j];
+                numTexts[i, j].ansFlg = true;
             }
         }
     }
@@ -313,7 +434,7 @@ public class NumberPlateManager : MonoBehaviour
     public void AnsChange()
     {
         selectedNumbers.Clear();
-        ansNum = ShuffleAns(defaultNum);
+        ansNum = ShuffleAns(NumTempInit());
         NumTextAns(ansNum);
         DrawText();
         SelectRandomNumbers();
@@ -330,6 +451,7 @@ public class NumberPlateManager : MonoBehaviour
             int j = selectedNumbers[i] / 9;
             int k = selectedNumbers[i] % 9;
             text[j, k].text = "";
+            numTexts[j, k].ansFlg = false;
         }
     }
 
@@ -339,10 +461,15 @@ public class NumberPlateManager : MonoBehaviour
     /// <param name="value"></param>
     public void SelectNumButton(int value)
     {
+        if (numText.ansFlg) return;
         numText.text.text = value.ToString();
         numText.text.color = Color.black;
         if (numText.ansInt != value) NumMiss();
-        else if (AnsChack()) GameClear();
+        else
+        {
+            numText.ansFlg = true;
+            if (AnsChack()) GameClear();
+        }
     }
 
     /// <summary>
@@ -360,11 +487,11 @@ public class NumberPlateManager : MonoBehaviour
     /// <returns></returns>
     private bool AnsChack()
     {
-        for(int i = 0; i < 9; i++)
+        for (int i = 0; i < 9; i++)
         {
-            for(int j = 0; j < 9; j++)
+            for (int j = 0; j < 9; j++)
             {
-                if (ansNum[i, j] != int.Parse(text[i, j].text)) return false;
+                if (!numTexts[i, j].ansFlg) return false;
             }
         }
 
